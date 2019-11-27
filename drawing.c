3c6083b0c4d48eae9a505f7c885fccd36ef17ad2
LukaVujcic <lukavujcic@hotmail.com>
2019-11-27 23:06:34 +0100

    Ubacena granica terena

--- drawing.c
+++ drawing.c
@@ -49,7 +49,7 @@ void draw_snake(Snake *snake)
         for (i=1;i<snake->size;i++)
         {
             /*Deo koda koji naizmenicno boji zmijicu*/
-            /*if (i&1)  
+           /* if (i&1)  
             {
                  glColor3f(0,0,1);
             }
@@ -85,24 +85,54 @@ static double function_plane(double u,double v)//funkcija je f(u,v)=c, zadaje ra
     double c=-0.5; // uzeto je 0.5 kao edge/2, jer je edge duzina ivice kocke
     return c;
 }
-void draw_terrain()
+static void draw_border_of_terrain(int U_FROM, int U_TO, int V_FROM, int V_TO)
+{
+    double edge=1;
+    glColor3f(1,0,0);
+
+    glPushMatrix(); //gore
+        glTranslatef((U_FROM+U_TO)/2,0,V_FROM+edge/2);
+        glScalef(U_TO-U_FROM,1,1);
+        glutSolidCube(edge);
+    glPopMatrix();
+    glPushMatrix(); //dole
+        glTranslatef(-(U_FROM+U_TO)/2,0,-V_FROM-edge/2);
+        glScalef(U_TO-U_FROM,1,1);
+        glutSolidCube(edge);
+    glPopMatrix();
+    glPushMatrix(); //levo
+        glTranslatef(U_FROM+edge/2,0,-(V_FROM+V_TO)/2);
+        glScalef(1,1,V_TO-V_FROM);
+        glutSolidCube(edge);
+    glPopMatrix();
+    glPushMatrix(); //desno
+        glTranslatef(-U_FROM-edge/2,0,(V_FROM+V_TO)/2);
+        glScalef(1,1,V_TO-V_FROM);
+        glutSolidCube(edge);
+    glPopMatrix();
+
+}
+static void draw_plane(int U_FROM, int U_TO, int V_FROM, int V_TO)
 {
     int u, v;
     glPushMatrix();
-
-    int U_FROM=-15;
-    int U_TO=15;
+        glColor3f(0,1,1);
+        for (u = U_FROM; u < U_TO; u++) { 
+            glBegin(GL_TRIANGLE_STRIP);
+            for (v = V_FROM; v <= V_TO; v++) {
+                set_vertex_and_normal(u, v, function_plane);
+                set_vertex_and_normal(u + 1, v, function_plane);
+            }
+            glEnd();
+        }
+    glPopMatrix();
+}
+void draw_terrain()
+{
+    int U_FROM=-17;
+    int U_TO=17;
     int V_FROM=-15;
     int V_TO=15;
-    glColor3f(0,1,1);
-    for (u = U_FROM; u < U_TO; u++) { 
-        glBegin(GL_TRIANGLE_STRIP);
-        for (v = V_FROM; v <= V_TO; v++) {
-            set_vertex_and_normal(u, v, function_plane);
-            set_vertex_and_normal(u + 1, v, function_plane);
-        }
-        glEnd();
-    }
-
-    glPopMatrix();
+    draw_plane(U_FROM,U_TO,V_FROM,V_TO); //crtamo ravan po kojoj se krecemo
+    draw_border_of_terrain(U_FROM,U_TO,V_FROM,V_TO); //crtamo okvir za teren 
 }
--- drawing.h
+++ drawing.h
@@ -5,5 +5,7 @@ void draw_coordinate_system(void); /*Postavljamo kordinatni sistem radi lakseg p
 void draw_snake(Snake *snake); /*Funkcija za iscrtavanje zmijice*/
 void draw_food(const Food *food); //Funkcija za crtanje hrane
 void set_vertex_and_normal(double u, double v,double (*function)(double,double)); //postavljanje normale i crtanje tacke po funkciji function
-void draw_terrain();
+static void draw_border_of_terrain(int U_FROM, int U_TO, int V_FROM, int V_TO); //crtanje granice terena
+static void draw_plane(int U_FROM, int U_TO, int V_FROM, int V_TO); //crtanje ravni
+void draw_terrain(); //crtanje terena
 #endif
